32eb18600089740b703581ebb833ffb1b6d32e21
ankitanand200193 <ankit200193@gmail.com>
2025-05-20 18:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def delete_student(student_id):
 # Flask routes
 @app.route('/')
 def home():
-    return "Welcome to the Ankit Student Management System API!", 200
+    return "Welcome to the Student Management System API!", 200
 
 @app.route('/students', methods=['POST'])
 def add():
